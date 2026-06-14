8905e0ad46d935f9b4da2c42e04aec7fec1189bc
Mordy Pesach <mordypassover@gmail.com>
2026-06-14 16:55:04 +0300

    include member router in main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ create_books_and_members_tables()
 
 app = FastAPI()
 app.include_router(book_routes.router)
+app.include_router(member_routes.router)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
