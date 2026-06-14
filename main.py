80e55910274e83318d7f531fa3c77cde644c0fff
Mordy Pesach <mordypassover@gmail.com>
2026-06-14 11:20:35 +0300

    update main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
 from routes import member_routes, book_routes, report_routes
+from database.db_connection import create_books_and_members_tables
+
+create_books_and_members_tables()
 
 app = FastAPI()
 app.include_router(book_routes.router)
